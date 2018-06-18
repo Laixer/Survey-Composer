d3602beeff65de23fc5d7fd13005ce2e1f5d56ee
yorickdewid <yorick17@outlook.com>
2018-06-18 12:48:29 +0200

    Empty defines

--- define.py
+++ define.py
@@ -9,7 +9,7 @@ externally before the application is loaded.
 from enum import Enum
 
 class Define(Enum):
-	APP_NAME = 'Testapp'
-	APP_URL = 'https://172.16.0.24'
+	APP_NAME = ''
+	APP_URL = ''
 	APP_THEME = ''
 
